f9ffbee767331281d6bcd9a68840aa4fce55f00d
Eric <errkkgeorge@gmail.com>
2012-06-12 15:32:16 +0100

    Filtering in admin

--- grindstone/admin.py
+++ grindstone/admin.py
@@ -1,8 +1,25 @@
 from models import ImportEvent, Interval, Task, TaskAlias
-
 from django.contrib import admin
 
-admin.site.register(ImportEvent)
-admin.site.register(Interval)
+class ImportEventAdmin(admin.ModelAdmin):
+	list_display = ('date', 'user')
+	list_filter = ('date', 'user')
+
+class IntervalAdmin(admin.ModelAdmin):
+	list_display = ('task','duration','user')
+	list_filter = ('alias',)
+
+	def task(self, obj):
+		return obj.alias.task
+
+	def user(self, obj):
+		return obj.alias.user
+
+class TaskAliasAdmin(admin.ModelAdmin):
+	list_display = ('string','task','user')
+	list_filter = ('user','task')
+
+admin.site.register(ImportEvent, ImportEventAdmin)
+admin.site.register(Interval,IntervalAdmin)
 admin.site.register(Task)
-admin.site.register(TaskAlias)
+admin.site.register(TaskAlias,TaskAliasAdmin)
