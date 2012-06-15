55efb2901338abae7fa3593479e0ae8e692b2293
Eric <errkkgeorge@gmail.com>
2012-06-15 16:27:26 +0100

    check user is logged in before doing the import check

--- grindstone/processors.py
+++ grindstone/processors.py
@@ -6,14 +6,15 @@ from django.contrib import messages
 
 
 def nag(request):
+
 	today = datetime.today()
 	td_week = timedelta(weeks=1)
 	try:
 		recent_imports = ImportEvent.objects.filter( date__lte=today, date__gte=today-td_week, user=request.user )
 	except:
 		recent_imports = False
-		
-	if not bool(recent_imports):
+
+	if not bool(recent_imports) and request.user.is_authenticated():
 		messages.add_message(request, messages.WARNING, 'Naughty! you havent uploaded your XML this week')
 
 	return { 'recent_imports': recent_imports }
